bf24c5d301a5ed38773d9792e7263e83656af466
ZeroPointGaming <comfyseven4@gmail.com>
2020-04-20 13:43:16 -0400

    Init Commit
    initial upload

---
+++ InventoryManagmentSystem/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace InventoryManagmentSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.InventoryItemTotalValuetxt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.InventoryItemValuetxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.InventoryItemNametxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.InventoryItemQtytxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(282, 250);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Update Inventory Item";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.InventoryItemTotalValuetxt);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.InventoryItemValuetxt);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.InventoryItemNametxt);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.InventoryItemQtytxt);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(221, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(428, 279);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Inventory Item Manager";
+            // 
+            // InventoryItemTotalValuetxt
+            // 
+            this.InventoryItemTotalValuetxt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.InventoryItemTotalValuetxt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.InventoryItemTotalValuetxt.ForeColor = System.Drawing.Color.White;
+            this.InventoryItemTotalValuetxt.Location = new System.Drawing.Point(137, 252);
+            this.InventoryItemTotalValuetxt.Name = "InventoryItemTotalValuetxt";
+            this.InventoryItemTotalValuetxt.ReadOnly = true;
+            this.InventoryItemTotalValuetxt.Size = new System.Drawing.Size(142, 20);
+            this.InventoryItemTotalValuetxt.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(6, 255);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(127, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Value of Inventory Items: ";
+            // 
+            // InventoryItemValuetxt
+            // 
+            this.InventoryItemValuetxt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.InventoryItemValuetxt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.InventoryItemValuetxt.ForeColor = System.Drawing.Color.White;
+            this.InventoryItemValuetxt.Location = new System.Drawing.Point(84, 75);
+            this.InventoryItemValuetxt.Name = "InventoryItemValuetxt";
+            this.InventoryItemValuetxt.Size = new System.Drawing.Size(100, 20);
+            this.InventoryItemValuetxt.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(6, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Item Price: ";
+            // 
+            // InventoryItemNametxt
+            // 
+            this.InventoryItemNametxt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.InventoryItemNametxt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.InventoryItemNametxt.ForeColor = System.Drawing.Color.White;
+            this.InventoryItemNametxt.Location = new System.Drawing.Point(84, 23);
+            this.InventoryItemNametxt.Name = "InventoryItemNametxt";
+            this.InventoryItemNametxt.Size = new System.Drawing.Size(138, 20);
+            this.InventoryItemNametxt.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(6, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Item Name: ";
+            // 
+            // InventoryItemQtytxt
+            // 
+            this.InventoryItemQtytxt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.InventoryItemQtytxt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.InventoryItemQtytxt.ForeColor = System.Drawing.Color.White;
+            this.InventoryItemQtytxt.Location = new System.Drawing.Point(84, 49);
+            this.InventoryItemQtytxt.Name = "InventoryItemQtytxt";
+            this.InventoryItemQtytxt.Size = new System.Drawing.Size(100, 20);
+            this.InventoryItemQtytxt.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(6, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Item Quantity: ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.listBox1);
+            this.groupBox2.ForeColor = System.Drawing.Color.White;
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(193, 533);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Inventory Items";
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Location = new System.Drawing.Point(107, 480);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 47);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Save Inventory Items";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(6, 480);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 47);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Delete Inventory Item";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listBox1.ForeColor = System.Drawing.Color.White;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.IntegralHeight = false;
+            this.listBox1.Location = new System.Drawing.Point(6, 19);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(181, 455);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.ClientSize = new System.Drawing.Size(661, 557);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.MinimumSize = new System.Drawing.Size(677, 342);
+            this.Name = "Form1";
+            this.Text = "Inventory Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox InventoryItemTotalValuetxt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox InventoryItemValuetxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox InventoryItemNametxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox InventoryItemQtytxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ InventoryManagmentSystem/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace InventoryManagmentSystem
+{
+    public partial class Form1 : Form
+    {
+        public InventoryItems InventoryManager = new InventoryItems();
+        public string SaveFile = Directory.GetCurrentDirectory() + "/Inventory.dat";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //When the main form is loaded at the start of the program, deserialize the stored inventory data and load it into the program.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!File.Exists(SaveFile))
+                {
+                    
+                }
+                else
+                {
+                    BinaryFormatter SaveManager = new BinaryFormatter();
+                    FileStream FileManager = new FileStream(SaveFile, FileMode.Open);
+                    InventoryManager = (InventoryItems)SaveManager.Deserialize(FileManager);
+
+                    foreach (KeyValuePair<string, InventoryItem> InventoryItem in InventoryManager.LocalInventory)
+                    {
+                        listBox1.Items.Add(InventoryItem.Key);
+                    }
+
+                    FileManager.Close();
+                    FileManager.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        //When the listbox selection is changed, update the items information in the inventory management screen.
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                InventoryItemNametxt.Text = InventoryManager.LocalInventory[listBox1.SelectedItem.ToString()].ItemName;
+                InventoryItemQtytxt.Text = InventoryManager.LocalInventory[listBox1.SelectedItem.ToString()].ItemCount.ToString();
+                InventoryItemValuetxt.Text = InventoryManager.LocalInventory[listBox1.SelectedItem.ToString()].ItemCost.ToString();
+                InventoryItemTotalValuetxt.Text = InventoryManager.LocalInventory[listBox1.SelectedItem.ToString()].InventoryItemsValue.ToString();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        //Resaves the inventory item or adds a new item to the local inventory.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listBox1.SelectedItem == null)
+                {
+                    InventoryItem NewInventoryItem = new InventoryItem();
+                    NewInventoryItem.ItemName = InventoryItemNametxt.Text;
+                    NewInventoryItem.ItemCount = float.Parse(InventoryItemQtytxt.Text);
+                    NewInventoryItem.ItemCost = float.Parse(InventoryItemValuetxt.Text);
+                    NewInventoryItem.InventoryItemsValue = NewInventoryItem.ItemCost * NewInventoryItem.ItemCount;
+
+                    InventoryItemNametxt.Text = "";
+                    InventoryItemQtytxt.Text = "";
+                    InventoryItemValuetxt.Text = "";
+                    InventoryItemTotalValuetxt.Text = "";
+
+                    InventoryManager.LocalInventory.Add(NewInventoryItem.ItemName, NewInventoryItem);
+
+                    listBox1.Items.Add(NewInventoryItem.ItemName.ToString());
+                }
+                else
+                {
+                    InventoryItem NewInventoryItem = new InventoryItem();
+
+                    NewInventoryItem.ItemName = InventoryItemNametxt.Text;
+                    NewInventoryItem.ItemCount = float.Parse(InventoryItemQtytxt.Text);
+                    NewInventoryItem.ItemCost = float.Parse(InventoryItemValuetxt.Text);
+                    NewInventoryItem.InventoryItemsValue = NewInventoryItem.ItemCost * NewInventoryItem.ItemCount;
+
+                    InventoryItemNametxt.Text = "";
+                    InventoryItemQtytxt.Text = "";
+                    InventoryItemValuetxt.Text = "";
+                    InventoryItemTotalValuetxt.Text = "";
+
+                    InventoryManager.LocalInventory[listBox1.SelectedItem.ToString()] = NewInventoryItem;
+                }
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+
+        //Save the inventory items object to file.
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (File.Exists(SaveFile))
+                {
+                    BinaryFormatter SaveManager = new BinaryFormatter();
+                    FileStream FileManager = new FileStream(SaveFile, FileMode.Open);
+
+                    SaveManager.Serialize(FileManager, InventoryManager);
+
+                    FileManager.Close();
+                    FileManager.Dispose();
+                }
+                else
+                {
+                    BinaryFormatter SaveManager = new BinaryFormatter();
+                    FileStream NewFileManager = new FileStream(SaveFile, FileMode.OpenOrCreate);
+
+                    SaveManager.Serialize(NewFileManager, InventoryManager);
+
+                    NewFileManager.Close();
+                    NewFileManager.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+
+        //Delete the inventory item that is currently selected.
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                InventoryManager.LocalInventory.Remove(listBox1.SelectedItem.ToString());
+                listBox1.Items.Remove(listBox1.SelectedItem);
+                InventoryItemNametxt.Text = "";
+                InventoryItemQtytxt.Text = "";
+                InventoryItemValuetxt.Text = "";
+                InventoryItemTotalValuetxt.Text = "";
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+    }
+
+    [Serializable]
+    public class InventoryItems
+    {
+        public Dictionary<string, InventoryItem> LocalInventory = new Dictionary<string, InventoryItem>();
+    }
+
+    //Structure to hold the data of each inventory item.
+    [Serializable]
+    public struct InventoryItem
+    {
+        public string ItemName;
+        public float ItemCount;
+        public float ItemCost;
+        public float InventoryItemsValue;
+    }
+}
